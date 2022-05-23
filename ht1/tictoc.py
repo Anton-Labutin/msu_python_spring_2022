8373d397445906d276d38d1fe7207e5cf68c27b7
Anton Labutin <Anton.Labutin.1997@yandex.ru>
2022-05-23 21:46:40 +0300

    init

---
+++ ht1/tictoc.py
@@ -0,0 +1,20 @@
+
+
+class TicTacGame:
+
+    def show_board():
+        pass
+
+    def validate_input():
+        pass
+
+    def start_game():
+        
+
+    def check_winner():
+        pass
+
+
+if __name__ == "__main__":
+    game = TicTacGame()
+    game.start_game()
